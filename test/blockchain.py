4723c1af54a0b04c8ecae43789558353e6a4f5de
caldwelljt2 <caldwelljt@airboxinc.com>
2022-12-31 17:45:34 -0500

    starting classwork

---
+++ test/blockchain.py
@@ -0,0 +1,38 @@
+blockchain = [0]
+
+print(blockchain)
+
+# blockchain.append(blockchain[-1])
+
+# print(blockchain)
+
+
+def get_last_blockchain_value():
+    if blockchain != []:
+        return blockchain[-1]
+    return [0] #incase this is the first block
+
+
+def add_value(transaction_amount):
+    blockchain.append([get_last_blockchain_value(), transaction_amount])
+    # print("blockchain contents:",blockchain)
+
+
+# add_value(3)
+# add_value(5.2)
+# add_value(1.4)
+
+def get_user_input():
+    return float(input('what about do you wish to add to the blockchain?'))
+
+add_value(get_user_input())
+add_value(get_user_input())
+add_value(get_user_input())
+
+# tx_amount = float(input('what about do you wish to add to the blockchain?'))
+# add_value(tx_amount)
+
+# tx_amount = float(input('what about do you wish to add to the blockchain?'))
+# add_value(tx_amount)
+
+print(blockchain)
