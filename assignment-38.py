5ffaa730f0ad4ef2996b3a4ad8745ed009fa0c0e
caldwelljt2 <caldwelljt@airboxinc.com>
2023-01-01 12:16:46 -0500

    downloaded resources

---
+++ assignment-38.py
@@ -0,0 +1,23 @@
+#1 Create two variables – one with your name and one with your age
+my_name = input("Name: ")
+my_age = input("Age: ")
+ 
+#2 Create a function which prints your data as one string
+def print_data(name, age):
+    print(name + " " + age)
+ 
+#3 Create a function which prints ANY data (two arguments) as one string
+def print_any_data(val1, val2):
+    print(str(val1) + str(val2))
+    
+#4 Create a function which calculates and returns the number of decades you already lived (e.g. 23 = 2 decades)
+def decades(yrs):
+    print(f"You have been alive { int(yrs) % 10 } decades")
+ 
+print_data(my_name, my_age)
+print_any_data(my_name, my_age)
+print_any_data("string", 1)
+print_any_data(True, 0.55)
+print_any_data([1,2], (3,4))
+print_any_data({'diction':'ary'}, False)
+decades(my_age)
--- test/blockchain.py
+++ test/blockchain.py
@@ -1,3 +1,4 @@
+# instalize blockchain
 blockchain = []
 
 print(blockchain)
@@ -8,12 +9,14 @@ print(blockchain)
 
 
 def get_last_blockchain_value():
+    """ returns the last blockchain value or [0] if none exists"""
     if blockchain != []:
         return blockchain[-1]
     return [0] #incase this is the first block
 
 
 def add_value(transaction_amount):
+    """ add a transaction to the blockchain"""
     blockchain.append([get_last_blockchain_value(), transaction_amount])
     # print("blockchain contents:",blockchain)
 
@@ -23,10 +26,12 @@ def add_value(transaction_amount):
 # add_value(1.4)
 
 def get_user_input():
-    transaction_amount = input('what about do you wish to add to the blockchain?')
+    """ gets user input recursivly until input is valid for return"""
+    transaction_amount = input('what about do you wish to add to the blockchain? ')
     try:
         return float(transaction_amount)
     except:
+        print("Invalid input, try again (must be a usable number.... ")
         get_user_input()
         # print('your blockchain was corrupted with an unusable value')
         # return None 
@@ -43,3 +48,4 @@ add_value(get_user_input())
 # add_value(tx_amount)
 
 print(blockchain)
+print("Final input ",blockchain[-1][-1])
